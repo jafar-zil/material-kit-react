c6187b5f493a84ad61ea83a000f3480b2279ca09
Jafar <jafar@sellowpay.com>
2024-07-20 17:53:59 +0530

    item action added

--- src/sections/item/item-table-row.jsx
+++ src/sections/item/item-table-row.jsx
@@ -6,6 +6,8 @@ import Checkbox from '@mui/material/Checkbox';
 import TableCell from '@mui/material/TableCell';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Popover from '@mui/material/Popover';
 import Iconify from 'src/components/iconify';
 
 ItemTableRow.propTypes = {
@@ -27,6 +29,10 @@ export default function ItemTableRow({
     setOpenPopover(event.currentTarget);
   };
 
+  const handleClosePopover = () => {
+    setOpenPopover(null);
+  };
+
   return (
     <TableRow hover selected={selected}>
       <TableCell padding="checkbox">
@@ -46,8 +52,25 @@ export default function ItemTableRow({
           <Iconify icon="eva:more-vertical-fill" />
         </IconButton>
       </TableCell>
-
-     
+      <Popover
+        open={Boolean(openPopover)}
+        anchorEl={openPopover}
+        onClose={handleClosePopover}
+        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        PaperProps={{
+          sx: { width: 160 },
+        }}
+      >
+        <MenuItem onClick={handleClosePopover}>
+          <Iconify icon="eva:edit-fill" sx={{ mr: 2 }} />
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleClosePopover} sx={{ color: 'error.main' }}>
+          <Iconify icon="eva:trash-2-outline" sx={{ mr: 2 }} />
+          Delete
+        </MenuItem>
+      </Popover>
     </TableRow>
   );
 }

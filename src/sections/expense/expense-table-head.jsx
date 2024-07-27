bd6322ed628376352ca9a0f977f0cefab499c599
Jafar <jafar@sellowpay.com>
2024-07-27 11:06:29 +0530

    expense date picker updates

--- src/sections/expense/expense-table-head.jsx
+++ src/sections/expense/expense-table-head.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import { Box, TableRow, TableCell, TableHead, TableSortLabel, TextField, Autocomplete, IconButton } from '@mui/material';
-import { DatePicker } from '@mui/x-date-pickers';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import Iconify from 'src/components/iconify';
 import { useState } from 'react';
 import { format } from 'date-fns';
@@ -113,40 +113,60 @@ export default function ExpenseTableHead({
                     case 'date':
                       return (
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                          label="Search"
-                          value={filterValues[headCell.id] || null}
-                          onChange={(newValue) => handleFilterChange(headCell.id)(null, newValue)}
-                          format="yyyy-MM-dd"
-                          slots={{
-                            textField: (params) => (
-                              <TextField
-                                {...params}
-                                variant="standard"
-                                size="small"
-                                fullWidth
-                                InputProps={{
-                                  ...params.InputProps,
-                                  endAdornment: (
-                                    <>
-                                      {filterValues[headCell.id] ? (
-                                        <IconButton
-                                          onClick={handleClearFilter(headCell.id)}
-                                          size="small"
-                                        >
-                                          <Iconify icon="eva:close-fill" />
-                                        </IconButton>
-                                      ) : (
-                                        params.InputProps.endAdornment
-                                      )}
-                                    </>
-                                  ),
-                                }}
-                              />
-                            ),
-                          }}
-                        />
-                      </LocalizationProvider>
+                          <DesktopDatePicker
+                            label="Search"
+                            sx={{
+                              width: {
+                                xs: 100,  // Width for extra-small screens (mobile)
+                                sm: 150,  // Width for small screens and above (desktop)
+                              },
+                            }}
+                            value={filterValues[headCell.id] || null}
+                            onChange={(newValue) => handleFilterChange(headCell.id)(null, newValue)}
+                            format="yyyy-MM-dd"
+                            slotProps={{
+                              field: { clearable: true, onClear: () => handleClearFilter(headCell.id) },
+                            }}
+                            slots={{
+                              textField: (params) => (
+                                <TextField
+                                  {...params}
+                                  InputProps={{
+                                    ...params.InputProps,
+                                    sx: {
+                                      '& .MuiInputBase-input': {
+                                        fontSize: {xs : '0.6rem',sm : '0.875rem'},
+                                        height: '1.5em',
+                                        padding: {sm :'6px 8px',xs: '3px 4px'}, // Adjust padding to ensure proper spacing
+                                      },
+                                      '& .MuiOutlinedInput-root': {
+                                        padding: '0 8px', // Ensure outer padding aligns with input
+                                      },
+                                      '& .MuiSvgIcon-root': {
+                                        fontSize: {xs : '0.6rem',sm : '1rem'}, // Adjust icon size
+                                        marginRight: '0px', // Reduce margin between icon and text
+                                      },
+                                      '& .MuiIconButton-root': {
+                                        padding: '2px', // Adjust icon size
+                                      },
+                                    },
+                                  }}
+                                  InputLabelProps={{
+                                    ...params.InputLabelProps,
+                                    sx: {
+                                      fontSize: '0.8em', // Adjust font size
+                                      top: '2px', // Adjust top position of the label
+                                      transform: 'translate(14px, 8px) scale(1)', // Ensure the label is positioned correctly
+                                      '&.MuiInputLabel-shrink': {
+                                        transform: 'translate(14px, -6px) scale(0.75)', // Adjust for shrunk label state
+                                      },
+                                    },
+                                  }}
+                                />
+                              )
+                            }}
+                          />
+                        </LocalizationProvider>
                       );
                     default:
                       return (
